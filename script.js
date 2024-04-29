0a6b36520ebf57b892d2c10db4e82232c8e2303d
Khab <unitetempete@gmail.com>
2024-04-29 23:52:48 +0200

    Creating a grid item is working, now beginning looping to make a full grid

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let grid = document.querySelector(".grid-container");
 
-console.log(grid.textContent)
+function createGridItem(grid){
+    let element = document.createElement("div");
+    element.style.backgroundColor = "blue";
+    element.style.height = "20px";
+    element.style.width = "20px";
+    grid.appendChild(element);
+}
+
+createGridItem(grid);
