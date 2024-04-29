eeafd5aecde4bf8d4754803338e38996d1b58c4e
Khab <unitetempete@gmail.com>
2024-04-30 00:44:27 +0200

    Cells are now occuping all the space in a grid manner.

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 let grid = document.querySelector(".grid-container");
+const NUMBER_OF_GRID_ELEMENTS = 16;
+const SIZE_FOR_GRID_ELEMENT = 100 / NUMBER_OF_GRID_ELEMENTS;
+
+
 
 function createGridItem(grid){
     let element = document.createElement("div");
+    element.style.border = "1px solid black";
+    element.style.backgroundColor = "blue";
+    element.style.width = SIZE_FOR_GRID_ELEMENT+"%";
+    element.style.height = SIZE_FOR_GRID_ELEMENT+"%";
     grid.appendChild(element);
 }
 
-for (let row = 1; row < 16; row++){
-    for(let col = 1; col < 16; col++){
+for (let row = 0; row < NUMBER_OF_GRID_ELEMENTS; row++){
+    for(let col = 0; col < NUMBER_OF_GRID_ELEMENTS; col++){
         createGridItem(grid);
     }
 }
+
+// let gridItems = document.querySelectorAll(".grid-container *");
+
+// gridItems.forEach(function(item) {
+//     item.addEventListener("mouseover", (event) => {
+//         event.currentTarget.style.backgroundColor = "black";
+//     });
+// });
